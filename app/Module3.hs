1688a82600513ef27c7c590d90d8c9e0ff56c143
TwITe <dm.blink19@gmail.com>
2018-12-09 16:22:44 +0300

    Update

---
+++ app/Module3.hs
@@ -0,0 +1,29 @@
+module Module3 where
+
+-- Работа со списками
+
+-- Реализуйте функцию addTwoElements, которая бы добавляла два переданных ей значения в голову переданного списка.
+-- https://stepik.org/lesson/8326/step/3?unit=1474
+addTwoElements :: a -> a -> [a] -> [a]
+addTwoElements arg1 arg2 list = arg1 : arg2 : list
+
+--
+--
+-- Реализуйте функцию nTimes, которая возвращает список, состоящий из повторяющихся значений ее первого аргумента.
+-- Количество повторов определяется значением второго аргумента этой функции.
+nTimes:: a -> Int -> [a]
+nTimes x n = nTimesHelper 0 []
+  where
+    nTimesHelper cnt list | cnt == n = list
+                          | otherwise = nTimesHelper (cnt + 1) (x : list)
+--
+--
+-- Сформируйте список целых чисел, содержащий только те элементы исходного списка, значение которых нечетно.
+oddsOnly :: Integral a => [a] -> [a]
+oddsOnly xs = oddsOnlyHelper xs []
+  where
+    oddsOnlyHelper [] ans = reverseList [] ans
+    oddsOnlyHelper (x:xs) ans | odd x = oddsOnlyHelper xs (x : ans)
+                              | otherwise = oddsOnlyHelper xs ans
+    reverseList rxs [] = rxs
+    reverseList rxs (x:xs) = reverseList (x:rxs) xs
