48272108076eae72975705c1da1d8f116203b83d
blinky-z <dm.blink19@gmail.com>
2018-12-15 03:52:45 +0300

    Update

--- app/Module3.hs
+++ app/Module3.hs
@@ -1,4 +1,4 @@
-module Module3 where
+﻿module Module3 where
 
 -- Работа со списками
 
@@ -41,6 +41,38 @@ isPalindrome (x:list) | x == last list = isPalindrome (init list)
 
 isPalindrome' :: Eq a => [a] -> Bool
 isPalindrome' xs = xs == reverse xs
+
+--
+--
+-- Составьте список сумм соответствующих элементов трех заданных списков
+-- https://stepik.org/lesson/8326/step/12?unit=1474
+sum3 :: Num a => [a] -> [a] -> [a] -> [a]
+sum3 xs ys zs = sum3Helper [] 0
+  where
+    maxn = max (max (length xs) (length ys)) (length zs)
+    sum3Helper res curn | curn == maxn = reverse res
+                        | otherwise = sum3Helper ((getElem xs curn 0 + getElem ys curn 0 + getElem zs curn 0) : res) (curn + 1)
+
+    getElem [] n curn = 0
+    getElem (x:xs) n curn | curn == n = x
+                          | otherwise = getElem xs n (curn + 1)
+
+--
+--
+-- Напишите функцию groupElems которая группирует одинаковые элементы в списке
+-- https://stepik.org/lesson/8326/step/13?unit=1474
+groupElems :: Eq a => [a] -> [[a]]
+groupElems [] = []
+groupElems xs = groupElemsHelper xs [] []
+  where
+    groupElemsHelper [x] curlist res
+      | null curlist = reverse ([x] : res)
+      | x == last curlist = reverse (curlist : res)
+      | otherwise = reverse ([x] : res)
+    groupElemsHelper (prev:xs) curlist res
+      | null curlist = groupElemsHelper (prev:xs) (prev : curlist) res
+      | head xs == prev = groupElemsHelper xs (head xs : curlist) res
+      | otherwise = groupElemsHelper xs [] (curlist : res)
 
 --
 --
