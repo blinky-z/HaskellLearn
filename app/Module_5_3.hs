9927f746fe3043f67057cc83e050cfe9f88d17bd
blinky-z <dm.blink19@gmail.com>
2019-01-08 16:22:31 +0300

    Update

--- app/Module_5_3.hs
+++ app/Module_5_3.hs
@@ -7,3 +7,23 @@ module Module_5_3 where
 --
 
 -- Identity - контейнер, который упаковывает любой базовый тип, не прибавляя никаких других свойств значению
+-- Identity есть и в стандартной библиотеке, но тут как обычно мы напишем реализацию сами
+newtype Identity a = Identity { runIdentity :: a }
+  deriving (Show, Eq)
+
+{-
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b
+-}
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+﻿
+instance Applicative Identity where
+  pure x = Identity x
+  Identity f <*> Identity v = Identity (f v)
+
+instance Monad Identity where
+  return x = Identity x {-упаковываем передаваемое значение в контейнер-}
+  Identity x >>= f = f x {-достаем значение из передаваемого контейнера и передаем это значение в стрелку Клейсли f-}
